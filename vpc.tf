ba7db35d7558a0ba1abab987d67dbe4b8a438d6a
kowshik48 <48944777+kowshik48@users.noreply.github.com>
2019-09-18 15:33:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    region  = "ap-southeast-1"
+    region  = "${var.region}"
     bucket  = "capgeminipoc-terraform-praveen-1"
     key     = "trss/terraform.tfstate"
     encrypt = true
